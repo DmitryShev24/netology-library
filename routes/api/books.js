9ed94518d38a1cf90337406251c52ea55421a6ce
Shevchenko Dmitry <dmitriy.e.shevchenko@rt.ru>
2021-09-30 08:23:00 +0300

    Mongo

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,116 +1,99 @@
 const express = require("express")
 const router = express.Router()
-const {Book} = require("../../models")
 const fileMiddleware = require('../../middleware/file')
 
-const stor = {
-    book: [],
-}
+const Book = require("../../models/book")
 
-let data = [1, 2, 3]
 
-data.map(el => {
-    const newBook = new Book(`book ${el}`, `desc book ${el}`);
-    stor.book.push(newBook);
+router.get("/", async (req, res) => {
+    const book = await Book.find().select('-__v');
+    res.json(book);
 });
 
 
-// router.post('/api/user/login', (req, res) => {
-//     res.status(201)
-//     res.json({
-//         id: 1, mail: "test@mail.ru"
+router.get('/:id', async (req, res) => {
+    const {id} = req.params;
+    try {
+        const book = await Book.findById(id).select('-__v');
+        res.json(book);
+    } catch (e) {
+        console.error(e);
+        res.status(404).json("book | not found");
+    }
+
+});
+
+// router.get("/:id/download", (req, res) => {
+//     res.download(__dirname+"/../public/book/2021-29-08-file.pdf", "file.pdf", err => {
+//         if (err){
+//             res.status(404).json()
+//         }
 //     })
-// });
+// })
 
-router.get("/", (req, res) => {
-    console.log('test')
-    const {book} = stor
-    res.json(book)
-});
+router.post('/', async (req, res) => {
+    //const {title, desc} = req.body
 
+    //const newBook = new Book({title, desc})
 
-router.get('/:id', (req, res) => {
-    const {book} = stor
-    const {id} = req.params
-    const idx = book.findIndex(el => el.id === id)
+    const newBook = new Book({
+        title: 'title...',
+        desc: 'desc...',
+    });
 
-    if (idx !== -1 ) {
-        res.json(book[idx])
-    } else {
-        res.status(404)
-        res.json("book | not found")
+    try {
+        await newBook.save();
+        res.status(201)
+        res.json(newBook);
+    } catch (e) {
+        console.error(e);
+        res.status(500).json();
     }
 });
 
-router.get("/:id/download", (req, res) => {
-    res.download(__dirname+"/../public/book/2021-29-08-file.pdf", "file.pdf", err => {
-        if (err){
-            res.status(404).json()
-        }
-    })
-})
+// router.post('/upload-book', fileMiddleware.single('book'), (req, res) => {
+//     if (req.file) {
+//         const {path} = req.file
+//         console.log(path)
+//
+//         res.json(path)
+//     } else {
+//         res.json(null)
+//     }
+// })
 
-router.post('/', (req, res) => {
-    const {book} = stor
-    const {title, desc} = req.body
-
-    const newBook = new Book(title, desc)
-    book.push(newBook);
-
-    res.status(201)
-    res.json(newBook)
-});
-
-router.post('/upload-book', fileMiddleware.single('book'), (req, res) => {
-    if (req.file) {
-        const {path} = req.file
-        console.log(path)
-
-        res.json(path)
-    } else {
-        res.json(null)
-    }
-})
-
-router.put('/:id', (req, res) => {
-    const {book} = stor;
+router.put('/:id', async (req, res) => {
     const {title, desc} = req.body;
     const {id} = req.params;
-    const idx = book.findIndex(el => el.id === id);
 
-    if (idx !== -1) {
-        book[idx] = {
-            ...book[idx],
-            title,
-            desc,
-        };
-        res.json(book[idx]);
-    } else {
-        res.status(404);
-        res.json("book | not found");
+    try {
+        await Book.findByIdAndUpdate(id, {title, desc});
+        res.redirect(`/api/books/${id}`);
+    } catch (e) {
+        console.error(e);
+        res.status(500).json();
     }
+
 });
 
-router.delete('/:id', (req, res) => {
-    const {book} = stor;
+router.delete('/:id', async (req, res) => {
     const {id} = req.params;
-    const idx = book.findIndex(el => el.id === id);
-
-    if (idx !== -1) {
-        book.splice(idx, 1);
+    try {
+        await Book.deleteOne({_id: id});
         res.json(true);
-    } else {
-        res.status(404);
-        res.json("book | not found");
+    } catch (e) {
+        console.error(e);
+        res.status(500).json();
     }
+
 });
 
-router.get('/:id/download-book', (req, res) => {
-    res.download(__dirname+'/../public/bookfile/2021-08-31T14-23-37.743Z-testfile.pdf', 'book.pdf', err=>{
-        if (err){
-            res.status(404).json();
-        }
-    });
-});
+// router.get('/:id/download-book', (req, res) => {
+//     res.download(__dirname+'/../public/bookfile/2021-08-31T14-23-37.743Z-testfile.pdf', 'book.pdf', err=>{
+//         if (err){
+//             res.status(404).json();
+//         }
+//     });
+// });
 
 module.exports = router
--- routes/books.js
+++ routes/books.js
@@ -1,99 +1,99 @@
-const express = require("express")
-const router = express.Router()
-const {Book} = require("../models")
-
-const stor = {
-    book: [],
-}
-
-let data = [1, 2, 3]
-
-data.map(el => {
-    const newBook = new Book(`book ${el}`, `desc book ${el}`);
-    stor.book.push(newBook);
-});
-
-router.get('/', (req, res) => {
-    const {book} = stor
-    const {title, desc} = req.body
-
-    res.render("books/index", {
-        title: "Книги",
-        books: book
-    })
-});
-
-router.get('/create', (req, res) => {
-        res.render('books/create', {
-            title: 'Создание книги',
-            book: {}
-        })
-    }
-)
-
-router.post('/create', (req, res) => {
-    const {book} = stor
-    const {title, desc} = req.body
-
-    const newBook = new Book(title, desc)
-    book.push(newBook);
-
-    res.status(201)
-    res.redirect('/books')
-})
-
-router.get('/:id', (req, res) => {
-    const {book} = stor
-    const {id} = req.params
-    const idx = book.findIndex(el => el.id === id);
-
-    res.render("books/view", {
-        title: "Книга | просмотр",
-        book: book[idx]
-    })
-});
-
-router.get('/update/:id', (req, res) => {
-    const {book} = stor
-    const {id} = req.params
-    const idx = book.findIndex(el => el.id === id);
-
-    res.render("books/update", {
-        title: "Книга | редактирование",
-        book: book[idx]
-    })
-});
-
-router.post('/update/:id', (req, res) => {
-    const {book} = stor
-    const {id} = req.params
-    const {title, desc} = req.body
-    const idx = book.findIndex(el => el.id === id);
-
-    if (idx !== -1) {
-        book[idx] = {
-            ...book[idx],
-            title,
-            desc
-        }
-        res.redirect(`/books/${id}`)
-    } else {
-        res.status(404).redirect('/404')
-    }
-});
-
-router.post('/delete/:id', (req, res) => {
-    const {book} = stor
-    const {id} = req.params
-    const idx = book.findIndex(el => el.id === id);
-
-    if (idx !== -1) {
-        book.splice(idx, 1)
-        res.redirect(`/books`)
-    } else {
-        res.status(404).redirect('/404')
-    }
-
-})
-
-module.exports = router
+// const express = require("express")
+// const router = express.Router()
+// const {Book} = require("../models")
+//
+// const stor = {
+//     book: [],
+// }
+//
+// let data = [1, 2, 3]
+//
+// data.map(el => {
+//     const newBook = new Book(`book ${el}`, `desc book ${el}`);
+//     stor.book.push(newBook);
+// });
+//
+// router.get('/', (req, res) => {
+//     const {book} = stor
+//     const {title, desc} = req.body
+//
+//     res.render("books/index", {
+//         title: "Книги",
+//         books: book
+//     })
+// });
+//
+// router.get('/create', (req, res) => {
+//         res.render('books/create', {
+//             title: 'Создание книги',
+//             book: {}
+//         })
+//     }
+// )
+//
+// router.post('/create', (req, res) => {
+//     const {book} = stor
+//     const {title, desc} = req.body
+//
+//     const newBook = new Book(title, desc)
+//     book.push(newBook);
+//
+//     res.status(201)
+//     res.redirect('/books')
+// })
+//
+// router.get('/:id', (req, res) => {
+//     const {book} = stor
+//     const {id} = req.params
+//     const idx = book.findIndex(el => el.id === id);
+//
+//     res.render("books/view", {
+//         title: "Книга | просмотр",
+//         book: book[idx]
+//     })
+// });
+//
+// router.get('/update/:id', (req, res) => {
+//     const {book} = stor
+//     const {id} = req.params
+//     const idx = book.findIndex(el => el.id === id);
+//
+//     res.render("books/update", {
+//         title: "Книга | редактирование",
+//         book: book[idx]
+//     })
+// });
+//
+// router.post('/update/:id', (req, res) => {
+//     const {book} = stor
+//     const {id} = req.params
+//     const {title, desc} = req.body
+//     const idx = book.findIndex(el => el.id === id);
+//
+//     if (idx !== -1) {
+//         book[idx] = {
+//             ...book[idx],
+//             title,
+//             desc
+//         }
+//         res.redirect(`/books/${id}`)
+//     } else {
+//         res.status(404).redirect('/404')
+//     }
+// });
+//
+// router.post('/delete/:id', (req, res) => {
+//     const {book} = stor
+//     const {id} = req.params
+//     const idx = book.findIndex(el => el.id === id);
+//
+//     if (idx !== -1) {
+//         book.splice(idx, 1)
+//         res.redirect(`/books`)
+//     } else {
+//         res.status(404).redirect('/404')
+//     }
+//
+// })
+//
+// module.exports = router
